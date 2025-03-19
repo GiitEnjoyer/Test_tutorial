fcefa251e8e654cbc444801c307cb39ff68fd3cb
GitEnjoer <zero021312@gmail.com>
2025-03-19 21:58:28 +0300

    1

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        assert true==true : "Что-то пошло не так, хотя нет, здесь не может пойти что-то не так";
+        assert true : "Что-то пошло не так, хотя нет, здесь не может пойти что-то не так";
         System.out.println("Все прошло замечательно");
 
     }
