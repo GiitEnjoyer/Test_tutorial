4dfec49a3fb5a9872dfffb1c037ff5226e7edb34
GitEnjoer <zero021312@gmail.com>
2025-03-27 15:40:05 +0300

    1

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        assert true : "Что-то пошло не так, хотя нет, здесь не может пойти что-то не так";
-        System.out.println("Все прошло замечательно");
-
-    }
-
-}
