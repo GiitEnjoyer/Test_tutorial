89e7c5d9da6f7febfccd50bf61bc2b911220004c
GitEnjoer <zero021312@gmail.com>
2025-04-03 14:45:50 +0300

    delete

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        assert true : "Что-то пошло не так, хотя нет, здесь не может пойти что-то не так";
+        System.out.println("Все прошло замечательно");
+
+    }
+
+}
